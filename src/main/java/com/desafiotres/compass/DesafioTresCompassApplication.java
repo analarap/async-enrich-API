a7a3dfba89ac6eb2541e5ed9d02761a65e6c0962
analuztx <analarapupo@gmail.com>
2023-08-07 14:18:39 -0300

    Main

---
+++ src/main/java/com/desafiotres/compass/DesafioTresCompassApplication.java
@@ -0,0 +1,13 @@
+package com.desafiotres.compass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTresCompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTresCompassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiotres/compass/DesafioTresCompassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiotres.compass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTresCompassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
